8179957af86045cfb014644cd4cf1906cfd0c85a
Vascan366 <107607527+Vascan366@users.noreply.github.com>
2022-06-16 12:28:40 +0530

    Added Thank you

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
     public void static main(String[] args){
       System.out.println("Hello World!!!");
       System.out.println("Welcome to Git Hub");
+      System.out.println("Thank you");
     }
   
 }  
